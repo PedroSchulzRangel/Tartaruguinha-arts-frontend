8e75c8c9b404dad999ab07c7221e8db0f83e34ea
mariaclaramaciel <mariaclaramaciel@icloud.com>
2021-12-09 18:36:26 -0300

    add commit responsivo

--- src/components/CarouselShowcase/index.jsx
+++ src/components/CarouselShowcase/index.jsx
@@ -5,15 +5,10 @@ export function CarouselShowcase() {
     return(
         <div className="container-products-showcase">
             <div className="container-destaque"> 
-                <h4 className="text-destaque">PRODUTOS EM DESTAQUE</h4>
+                    <h4 className="text-destaque">PRODUTOS EM DESTAQUE</h4>
             </div>
             <div className="category-container">
                 <div className="products-row">
-                    <div className="product-card">
-                        <div><img src="https://www.ecoloja.art.br/image/cache/catalog/Chaveiro/Chaveiro-Metal-Tartaruga-min-500x500.jpg" /></div>
-                        <div className="product-title">Caneca Tartaruguinha Azul</div>
-                        <div className="prize-tag"><div>R$ 15,00</div></div>
-                    </div>
                     <div className="product-card">
                         <div><img src="https://www.ecoloja.art.br/image/cache/catalog/Chaveiro/Chaveiro-Metal-Tartaruga-min-500x500.jpg" /></div>
                         <div className="product-title">Caneca Tartaruguinha Azul</div>
--- src/components/ProjectsSection/index.jsx
+++ src/components/ProjectsSection/index.jsx
@@ -6,16 +6,20 @@ export function ProjectsSection() {
     return (
         <div className="container-projects-section">
             <div className="card-text">
-                <h3 className="card-titulo-historia">PROJETOS</h3>
-                <p className="card-texto-historia">
-                    Por que o nome Tartaruguinha?
-                    Devem pensar que é por causa do projeto, mas não é. O nome Tartaruguinha Art’s nasceu do apelido da filha do nosso fundador e instrutor de mergulho Theo Costa. No processo de criar um nome para a loja, ele lembrou do apelido que deu quando a filha não tinha nem um ano de vida. E assim nasceu a Tartaruguinha Art’s!
-                </p>
-                <div className="button-saiba">
-                    <Button text="SAIBA MAIS" link="#"/>
+                <div>
+                <h3 className="card-titulo-historia one">PROJETOS</h3>
+                </div>
+                <div className="card-responsivo"> 
+                    <p className="card-texto-historia">
+                        Por que o nome Tartaruguinha?
+                        Devem pensar que é por causa do projeto, mas não é. O nome Tartaruguinha Art’s nasceu do apelido da filha do nosso fundador e instrutor de mergulho Theo Costa. No processo de criar um nome para a loja, ele lembrou do apelido que deu quando a filha não tinha nem um ano de vida. E assim nasceu a Tartaruguinha Art’s!
+                    </p>
+                    <div className="button-saiba">
+                        <Button text="SAIBA MAIS" link="#"/>
+                    </div>
                 </div>
             </div>
-            <div className="card-img-historia">
+            <div className="card-img-historia two">
                  <img src={Img} alt="imagem historia" />
             </div>
         </div>
