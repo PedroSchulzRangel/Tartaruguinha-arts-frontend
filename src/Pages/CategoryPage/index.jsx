512cc51f9b749a151707db335d12cda5e52d0ed4
mariaclaramaciel <mariaclaramaciel@icloud.com>
2021-12-11 21:57:27 -0300

    add pagina categoria

---
+++ src/Pages/CategoryPage/index.jsx
@@ -0,0 +1,17 @@
+
+import { IntroPage } from '../../components/IntroPage';
+import { Footer } from '../../components/Footer'
+import { ProductCategory } from '../../components/ProductCategory';
+
+
+export function CategoryPage() {
+
+    return (
+        <>
+            <IntroPage />
+            <ProductCategory />
+            <Footer />
+        </>
+    );
+
+}
